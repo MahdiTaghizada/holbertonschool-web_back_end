864e280284dbc871a71b5bf42ad3cfedf3868ca9
MahdiTaghizade <tagizademehti@gmail.com>
2026-01-18 14:30:39 +0400

    da

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""Deletion-resilient hypermedia pagination
-"""
+""" Deletion-resilient hypermedia pagination """
 
 import csv
 import math
@@ -16,10 +15,52 @@ class Server:
         self.__dataset = None
         self.__indexed_dataset = None
 
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0"""
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Silinmələrə qarşı davamlı səhifələmə metodu.
+        """
+        # Verilənləri indekslənmiş şəkildə əldə edirik
+        data_indexed = self.indexed_dataset()
+        
+        # İndeksin mövcudluğunu yoxlayırıq
+        assert index is not None and index >= 0 and index < len(data_indexed)
+
+        data = []
+        current_index = index
+        next_index = None
+        
+        # Lazımi miqdarda məlumatı toplayırıq (silinmiş indeksləri atlayaraq)
+        temp_index = index
+        while len(data) < page_size and temp_index < len(data_indexed):
+            item = data_indexed.get(temp_index)
+            if item:
+                data.append(item)
+            temp_index += 1
+            
+        next_index = temp_index if temp_index < len(data_indexed) else None
 
         return {
             "index": index,
-            "next_index": i,
-            "page_size": page_size,
-            "data": d
+            "next_index": next_index,
+            "page_size": len(data),
+            "data": data
         }
